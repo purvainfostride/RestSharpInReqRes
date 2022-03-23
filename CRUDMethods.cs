cc2a5129802cd525e485fa88feb35af342e18723
purvainfostride <purva@infostride.com>
2022-03-23 22:04:18 +0530

    Added DTO's, Get Method in CRUDMethods and tested in ValidatingResponces

---
+++ CRUDMethods.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json;
+using RestSharp;
+using RestSharpInReqRes.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace RestSharpInReqRes
+{
+    public class CRUDMethods
+    {
+        public ListOfUsersDTO GetUsers()
+        {
+            //REQUEST
+            var restClient = new RestClient("https://reqres.in/"); //creating var with RestClient method to pass base URL
+            var restRequest = new RestRequest("/api/users?page=2", Method.GET); //passing end point & method of CRUD
+            restRequest.AddHeader("Accept", "application/json"); //passing HEADERS
+            restRequest.RequestFormat = DataFormat.Json; //FORMAT
+
+            //RESPONSE
+            var response = restClient.Execute(restRequest);   //Handling response with interface by executing request
+            var content = response.Content; //getting content
+
+            //Deserialization   (Convert JSON ->.NET)
+            var users = JsonConvert.DeserializeObject<ListOfUsersDTO>(content);
+            return users;
+
+        }
+    }
+}
---
+++ ValidatingResponces.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace RestSharpInReqRes
+{
+    [TestClass]
+    public class ValidatingResponces
+    {
+        [TestMethod]
+        public void VerifyListOfUsers()
+        {
+            var demo = new CRUDMethods();
+            var response = demo.GetUsers(); //returning objects from the method
+            Assert.AreEqual(2, response.Page);
+            Assert.AreEqual("Michael", response.Data[0].first_name);
+            Assert.AreEqual("Lindsay", response.Data[1].first_name);
+        }
+    }
+}
